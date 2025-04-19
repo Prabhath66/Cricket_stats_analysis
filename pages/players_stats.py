909feb2495546254cc467e9bc7d536af0dad4ada
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-04-19 18:09:43 +0000

    Update pages/players_stats.py

--- pages/players_stats.py
+++ pages/players_stats.py
@@ -49,27 +49,21 @@ if player in list(set(batting['name'])) :
         with col0:
             if st.button(f"Bowling Stats of {player}", type="tertiary"):
                 st.write(bowling_stats)
-            
+                
+            # if st.toggle(f"Bowling Stats of {player}"):
+            #     st.write(bowling_stats)            
 
         
         col1, col2 = st.columns(2)
         with col1:
-            # if st.button(f"Batting Stats of {player}", type="tertiary"):
-            #     st.write(batting_stats)
-            
             # selecting the columns to analyze
-            col=st.selectbox("Select the Column to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
+            col=st.selectbox("Select the Column Name to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
             if col in batting_stats.columns:
               fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
               # fig.show() 
               st.plotly_chart(fig)
-        with col2:
-            # if st.button(f"Bowling Stats of {player}", type="tertiary"):
-            #     st.write(bowling_stats) 
-            
-            # if st.toggle(f"Bowling Stats of {player}"):
-            #     st.write(bowling_stats)
                 
+        with col2:                
             # selecting the columns to analyze
             col=st.selectbox("Select the Column to Analyze ", options=list(bowling_stats.columns), placeholder="Enter the Column Name to Analyze",)
             if col in bowling_stats.columns:
@@ -82,24 +76,6 @@ if player in list(set(batting['name'])) :
 
     # col=st.selectbox("Select the Column to Analyze ", options=list(batting_stats.columns), index=None, placeholder="Enter the Column Name to Analyze",)
 
-    # if col in batting_stats.columns:
-    #   fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
-    #   # fig.show() 
-    #   st.plotly_chart(fig)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -109,42 +85,37 @@ if player in list(set(batting['name'])) :
 # import pandas as pd
 # import plotly.express as px
 
-# batting = pd.read_csv("8teams_batting_stats.csv") 
-# bowling = pd.read_csv("8teams_bowling_stats.csv") 
+# teams_batting = pd.read_csv("total_teams_batting.csv")
+# teams_bowling = pd.read_csv("total_teams_bowling.csv")
+
+# teams=pd.concat([west_indies,england])
+# teams_bowl=pd.read_csv("teams_bowling_stats.csv")
+
+# #st.write(teams.groupby("Country").get_group("West Indies") )
 
 
-# # player selection 
-# player = st.selectbox("Select the Player", list(set(batting['name'])), index=None, placeholder="Enter the Player Name",) 
-# if player in list(set(batting['name'])) :
-#     #st.write("Batting Stats of {}".format(player))
-#     batting_stats=batting[batting['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
-#     #st.write(batting_stats)
-#     #st.write("Bowling Stats of {}".format(player))
-#     bowling_stats=bowling[bowling['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
-#     #st.write(bowling_stats) 
+# country = st.selectbox("Select the Country", list(teams.groupby("Country").groups.keys()), index=None, placeholder="Enter the Country Name",)
 
+#player = st.selectbox("Select the Player", list(teams.groupby("name").groups.keys()), index=None, placeholder="Enter the Player Name",)
 
-#     options = ["Bat", "Bowl", "Both"]
-#     selection = st.segmented_control(f"Stats of {player}", options, selection_mode="single")
-#     if selection == "Bat":
+# if country in list(teams.groupby("Country").groups.keys()):
+#     player_batting=teams.groupby("Country").get_group(country)
+#     player_bowling=teams_bowl.groupby("Country").get_group(country)
+#     player = st.selectbox("Select the Player", list(player_batting.groupby("name").groups.keys()), index=None, placeholder="Enter the Player Name",)
+#     if player in list(player_batting.groupby("name").groups.keys()) :
 #         st.write("Batting Stats of {}".format(player))
-#         st.write(batting_stats)
-#     elif selection == "Bowl": 
-#         st.write("Bowling Stats of {}".format(player)) 
-#         st.write(bowling_stats) 
-#     elif selection == "Both": 
-#         st.write("Batting Stats of {}".format(player))
+#         batting_stats=player_batting[player_batting['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T
 #         st.write(batting_stats)
 #         st.write("Bowling Stats of {}".format(player))
+#         bowling_stats=player_bowling[player_bowling['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T
 #         st.write(bowling_stats)
 
-#     select_stats=st.selectbox("Select the option (Batting / Bowling"),["Batting", "Bowling"],index)  
 
-#     col=st.selectbox("Select the Column to Analyze ", options=list(batting_stats.columns), index=None, placeholder="Enter the Column Name to Analyze",)
 
-#     if col in batting_stats.columns:
-#       fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
-#       # fig.show() 
-#       st.plotly_chart(fig)
-   
+
+# st.write(batting_stats["Matches"].values, batting_stats.index)
+# st.write(px.pie(data=batting_stats,values=batting_stats["Matches"].values, labels=batting_stats.index ))
+
+
+
     
