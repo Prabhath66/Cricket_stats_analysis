2fbadc10afda0a4f431d828954ef38f3254a4c80
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-04-20 04:40:43 +0000

    Update pages/players_stats.py

--- pages/players_stats.py
+++ pages/players_stats.py
@@ -6,71 +6,91 @@ batting = pd.read_csv("8teams_batting_stats.csv")
 bowling = pd.read_csv("8teams_bowling_stats.csv") 
 
 
-# player selection 
-player = st.selectbox("Select the Player", list(set(batting['name'])), index=None, placeholder="Enter the Player Name",) 
-if player in list(set(batting['name'])) :
-    #st.write("Batting Stats of {}".format(player))
-    batting_stats=batting[batting['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
-    #st.write(batting_stats)
-    #st.write("Bowling Stats of {}".format(player))
-    bowling_stats=bowling[bowling['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
-    #st.write(bowling_stats) 
+# #st.write(teams.groupby("Country").get_group("West Indies") )
 
 
-    options = ["Bat", "Bowl", "Both"]
-    selection = st.segmented_control(f"Stats of {player}", options, selection_mode="single")
-    if selection == "Bat":
-        if st.button(f"Batting Stats of {player}", type="tertiary"):
-            st.write(batting_stats)
-        
-        # selecting the columns to analyze
-        col=st.selectbox("Select the Column to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
-        if col in batting_stats.columns:
-          fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
-          # fig.show() 
-          st.plotly_chart(fig)
-            
-    elif selection == "Bowl": 
-        if st.button(f"Bowling Stats of {player}", type="tertiary"):
-            st.write(bowling_stats) 
-                
-        # selecting the columns to analyze
-        col=st.selectbox("Select the Column to Analyze ", options=list(bowling_stats.columns), placeholder="Enter the Column Name to Analyze",)
-        if col in bowling_stats.columns:
-          fig=px.pie(values=bowling_stats[col].values, names=bowling_stats.index, title=f'Bowling stats of {player} {col}') 
-          st.plotly_chart(fig)
+country = st.selectbox("Select the Country", list(batting.groupby("Country").groups.keys()), index=None, placeholder="Enter the Country Name",)
 
-    
-    elif selection == "Both":
-        col, col0 = st.columns(2)
-        with col:
+#player = st.selectbox("Select the Player", list(teams.groupby("name").groups.keys()), index=None, placeholder="Enter the Player Name",)
+
+if country in list(batting.groupby("Country").groups.keys()):
+    player_batting=batting.groupby("Country").get_group(country)
+    player_bowling=bowling.groupby("Country").get_group(country)
+    player = st.selectbox("Select the Player", list(player_batting.groupby("name").groups.keys()), index=None, placeholder="Enter the Player Name",)
+    if player in list(player_batting.groupby("name").groups.keys()) :
+        # st.write("Batting Stats of {}".format(player))
+        batting_stats=player_batting[player_batting['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T
+        # st.write(batting_stats)
+        # st.write("Bowling Stats of {}".format(player))
+        bowling_stats=player_bowling[player_bowling['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T
+        # st.write(bowling_stats)
+
+
+# # player selection 
+# player = st.selectbox("Select the Player", list(set(batting['name'])), index=None, placeholder="Enter the Player Name",) 
+# if player in list(set(batting['name'])) :
+#     #st.write("Batting Stats of {}".format(player))
+#     batting_stats=batting[batting['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
+#     #st.write(batting_stats)
+#     #st.write("Bowling Stats of {}".format(player))
+#     bowling_stats=bowling[bowling['name']==player][["ROWHEADER","Test","ODI","T20","IPL"]].set_index("ROWHEADER").T 
+#     #st.write(bowling_stats) 
+
+
+        options = ["Bat", "Bowl", "Both"]
+        selection = st.segmented_control(f"Stats of {player}", options, selection_mode="single")
+        if selection == "Bat":
             if st.button(f"Batting Stats of {player}", type="tertiary"):
                 st.write(batting_stats)
-        with col0:
-            if st.button(f"Bowling Stats of {player}", type="tertiary"):
-                st.write(bowling_stats)
-                
-            # if st.toggle(f"Bowling Stats of {player}"):
-            #     st.write(bowling_stats)            
-
-        
-        col1, col2 = st.columns(2)
-        with col1:
+            
             # selecting the columns to analyze
-            col=st.selectbox("Select the Column Name to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
+            col=st.selectbox("Select the Column to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
             if col in batting_stats.columns:
               fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
               # fig.show() 
               st.plotly_chart(fig)
                 
-        with col2:                
+        elif selection == "Bowl": 
+            if st.button(f"Bowling Stats of {player}", type="tertiary"):
+                st.write(bowling_stats) 
+                    
             # selecting the columns to analyze
             col=st.selectbox("Select the Column to Analyze ", options=list(bowling_stats.columns), placeholder="Enter the Column Name to Analyze",)
             if col in bowling_stats.columns:
-              fig=px.pie(values=bowling_stats[col].values, names=bowling_stats.index, title=f'Bowling stats of {player} {col}')
-              # fig.show() 
+              fig=px.pie(values=bowling_stats[col].values, names=bowling_stats.index, title=f'Bowling stats of {player} {col}') 
               st.plotly_chart(fig)
+
+    
+        elif selection == "Both":
+            col, col0 = st.columns(2)
+            with col:
+                if st.button(f"Batting Stats of {player}", type="tertiary"):
+                    st.write(batting_stats)
+            with col0:
+                if st.button(f"Bowling Stats of {player}", type="tertiary"):
+                    st.write(bowling_stats)
+                    
+                # if st.toggle(f"Bowling Stats of {player}"):
+                #     st.write(bowling_stats)            
+    
             
+            col1, col2 = st.columns(2)
+            with col1:
+                # selecting the columns to analyze
+                col=st.selectbox("Select the Column Name to Analyze ", options=list(batting_stats.columns), placeholder="Enter the Column Name to Analyze",)
+                if col in batting_stats.columns:
+                  fig=px.pie(values=batting_stats[col].values, names=batting_stats.index, title=f'Batting stats of {player} {col}')
+                  # fig.show() 
+                  st.plotly_chart(fig)
+                
+            with col2:                
+                # selecting the columns to analyze
+                col=st.selectbox("Select the Column to Analyze ", options=list(bowling_stats.columns), placeholder="Enter the Column Name to Analyze",)
+                if col in bowling_stats.columns:
+                  fig=px.pie(values=bowling_stats[col].values, names=bowling_stats.index, title=f'Bowling stats of {player} {col}')
+                  # fig.show() 
+                  st.plotly_chart(fig)
+                
 
     #select_stats=st.selectbox("Select the option (Batting / Bowling"),["Batting", "Bowling"],index)  
 
